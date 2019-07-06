76b0c4f404acec75020bd5a56f085c5d916d4666
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2019-07-06 00:50:34 -0400

    First two chapters.

---
+++ L3_firstProgram.py
@@ -0,0 +1,8 @@
+#This is a comment
+
+print('Hello world')
+print('What is your name')
+myName = input();
+print('Good to meet you, ' + myName)
+print('Length of name is: ' + (str(len(myName))))
+
---
+++ L5_IfStatement.py
@@ -0,0 +1,8 @@
+name = 'Bob'
+if name == 'Alice':
+    print('Hi Alice')
+elif name == 'Bob':
+    print('Hey Bob')
+else:
+    print('Wrong name')
+print('Done')
